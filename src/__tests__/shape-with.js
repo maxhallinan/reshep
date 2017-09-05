28a33cbf980a9c74d430be62509c8815774c46b5
Max Hallinan <maxhallinan@riseup.net>
2017-09-05 20:51:54 +0200

    Add tests for shapeWith

--- src/__tests__/shape-with.js
+++ src/__tests__/shape-with.js
@@ -1,4 +1,9 @@
+import { mount, } from 'enzyme';
+import 'jest-enzyme';
+import React from 'react';
 import { shapeWith, } from './../';
+import { path, } from './../util';
+
 
 describe(`reshep > shapeWith`, () => {
   test(`Throws a TypeError if the first argument is not an object.`, () => {
@@ -28,14 +33,106 @@ describe(`reshep > shapeWith`, () => {
     [ 1, null, undefined, [], {}, ].forEach(testErr);
   });
 
-  test(`Returns a higher-order component.`, () => {});
+  test(`Returns a higher-order component.`, () => {
+    const Base = () => React.createElement('p', {}, 'foo');
 
-  test(`Component maps deeply nested props for each entry in \`pathMap\`.`, () => {});
+    const HOC = shapeWith({})(Base);
 
-  test(`Component merges owner props and mapped props.`, () => {});
+    const wrapped = mount(React.createElement(HOC, props));
 
-  test(`Mapped props overwrteste by owner props.`, () => {});
+    expect(wrapped).toContainReact(React.createElement(Base));
+  });
 
-  test(`Component passes merged props to base component.`, () => {});
+  test(`Component maps deeply nested props for each entry in \`pathMap\`.`, () => {
+    const props = {
+      a: { b: { c: 1, }, },
+      d: { e: 2, },
+      f: 3,
+    };
+
+    const pathMap = {
+      'a.b.c': 'a',
+      'd.e': 'b.c',
+      'f': 'd.e.f',
+    };
+
+    const Base = jest.fn();
+    Base.mockReturnValue(null);
+
+    const HOC = shapeWith(pathMap)(Base);
+
+    mount(React.createElement(HOC, props));
+
+    const baseProps = Base.mock.calls[0][0];
+
+    Object.keys(pathMap)
+      .map((k) => [ k, pathMap[k], ])
+      .map((paths) => paths.map(p => p.split('.')))
+      .map(([ from, to, ]) =>
+        ([ path(from, props), path(to, baseProps), ]))
+      .forEach(([ from, to ]) => expect(from).toEqual(to));
+  });
+
+  test(`Component merges owner props and mapped props.`, () => {
+    const props = {
+      a: { b: { c: 1, }, },
+      d: { e: 2, },
+      f: 3,
+    };
+
+    const pathMap = {
+      'a.b.c': 'g',
+      'd.e': 'h.i',
+      'f': 'j.k.l',
+    };
+
+    const Base = jest.fn();
+    Base.mockReturnValue(null);
+
+    const HOC = shapeWith(pathMap)(Base);
+
+    mount(React.createElement(HOC, props));
+
+    const baseProps = Base.mock.calls[0][0];
+
+    const expected = {
+      ...props,
+      g: 1,
+      h: { i: 2 },
+      j: { k: { l: 3, }, },
+    };
+
+    expect(baseProps).toEqual(expected);
+  });
+
+  test(`Owner props are overwritten by mapped props.`, () => {
+    const props = {
+      a: { b: { c: 1, }, },
+      d: { e: 2, },
+      f: 3,
+    };
+
+    const pathMap = {
+      'a.b.c': 'a',
+      'f': 'd.e',
+    };
+
+    const Base = jest.fn();
+    Base.mockReturnValue(null);
+
+    const HOC = shapeWith(pathMap)(Base);
+
+    mount(React.createElement(HOC, props));
+
+    const baseProps = Base.mock.calls[0][0];
+
+    const expected = {
+      a: 1,
+      d: { e: 3, },
+      f: 3,
+    };
+
+    expect(baseProps).toEqual(expected);
+  });
 });
 
